b81270cf8c03f52918a7aa17f8b5b9c15284b70d
Joseph Lander <josephclander@gmail.com>
2022-08-19 15:40:11 +0100

    add author solution

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,16 +1,34 @@
+// const bubbleSort = (array) => {
+//   let reCheck = true;
+//   while (reCheck) {
+//     reCheck = false;
+//     let index = 0;
+//     while (index < array.length - 1) {
+//       if (array[index + 1] < array[index]) {
+//         reCheck = true;
+//         let temp = array[index];
+//         array[index] = array[index + 1];
+//         array[index + 1] = temp;
+//       }
+//       index++;
+//     }
+//   }
+//   return array;
+// };
+
+// the difference in this solution:
+// you know the first number has been checked on the first run
+// then the second number has been checked...
 const bubbleSort = (array) => {
-  let reCheck = true;
-  while (reCheck) {
-    reCheck = false;
-    let index = 0;
-    while (index < array.length - 1) {
-      if (array[index + 1] < array[index]) {
-        reCheck = true;
-        let temp = array[index];
-        array[index] = array[index + 1];
-        array[index + 1] = temp;
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (array[j] > array[j + 1]) {
+        //Swap the numbers
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
       }
-      index++;
     }
   }
   return array;
