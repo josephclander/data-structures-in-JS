66a3db11e9742f2d3fc86d9f37014b1b87309534
Joseph Lander <josephclander@gmail.com>
2022-08-19 12:49:53 +0100

    add fib iterative

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -15,7 +15,27 @@ const fibRecursive = (n) => {
 /**
  * iterative approach
  */
-const fibIterative = (n) => {};
+const fibIterative = (n) => {
+  //   if (n === 0) return 0;
+  //   if (n === 1) return 1;
+  //   let nMinus2 = 0;
+  //   let nMinus1 = 1;
+  //   let currentIndex = 1;
+  //   let answer = 1;
+  //   while (currentIndex !== n) {
+  //     answer = nMinus2 + nMinus1;
+  //     nMinus2 = nMinus1;
+  //     nMinus1 = answer;
+  //     currentIndex++;
+  //   }
+  //   return answer;
+  // the array is the array of answers with index
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr[n];
+};
 
 module.exports = {
   fibRecursive,
--- recursion/fibonacci.test.js
+++ recursion/fibonacci.test.js
@@ -1,23 +1,48 @@
 const { fibRecursive, fibIterative } = require("./fibonacci.js");
 
-test("fibRecursive(0)", () => {
-  expect(fibRecursive(0)).toBe(0);
+describe("recursive tests", () => {
+  test("fibRecursive(0)", () => {
+    expect(fibRecursive(0)).toBe(0);
+  });
+  test("fibRecursive(1)", () => {
+    expect(fibRecursive(1)).toBe(1);
+  });
+  test("fibRecursive(2)", () => {
+    expect(fibRecursive(2)).toBe(1);
+  });
+  test("fibRecursive(3)", () => {
+    expect(fibRecursive(3)).toBe(2);
+  });
+  test("fibRecursive(4)", () => {
+    expect(fibRecursive(4)).toBe(3);
+  });
+  test("fibRecursive(5)", () => {
+    expect(fibRecursive(5)).toBe(5);
+  });
+  test("fibRecursive(6)", () => {
+    expect(fibRecursive(6)).toBe(8);
+  });
 });
-test("fibRecursive(1)", () => {
-  expect(fibRecursive(1)).toBe(1);
-});
-test("fibRecursive(2)", () => {
-  expect(fibRecursive(2)).toBe(1);
-});
-test("fibRecursive(3)", () => {
-  expect(fibRecursive(3)).toBe(2);
-});
-test("fibRecursive(4)", () => {
-  expect(fibRecursive(4)).toBe(3);
-});
-test("fibRecursive(5)", () => {
-  expect(fibRecursive(5)).toBe(5);
-});
-test("fibRecursive(6)", () => {
-  expect(fibRecursive(6)).toBe(8);
+describe("iterative tests", () => {
+  test("fibIterative(0)", () => {
+    expect(fibIterative(0)).toBe(0);
+  });
+  test("fibIterative(1)", () => {
+    expect(fibIterative(1)).toBe(1);
+  });
+  test("fibIterative(2)", () => {
+    expect(fibIterative(2)).toBe(1);
+  });
+  test("fibIterative(3)", () => {
+    expect(fibIterative(3)).toBe(2);
+  });
+  test("fibIterative(4)", () => {
+    expect(fibIterative(4)).toBe(3);
+  });
+  test("fibIterative(5)", () => {
+    expect(fibIterative(5)).toBe(5);
+  });
+  test("fibIterative(6)", () => {
+    expect(fibIterative(6)).toBe(8);
+  });
 });
